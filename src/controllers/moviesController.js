5fe7299dcee8d9b8bdce939f03add2fbee49c3c6
GUIPARO <guisela2728@gmail.com>
2021-12-19 23:59:33 -0500

    Se realizacrud y relaciones

---
+++ src/controllers/moviesController.js
@@ -0,0 +1,111 @@
+const path = require('path');
+const db = require('../database/models');
+const sequelize = db.sequelize;
+const { Op } = require("sequelize");
+
+
+//Aqui tienen una forma de llamar a cada uno de los modelos
+// const {Movies,Genres,Actor} = require('../database/models');
+
+//Aquí tienen otra forma de llamar a los modelos creados
+const Movies = db.Movie;
+const Genres = db.Genre;
+const Actors = db.Actor;
+
+
+const moviesController = {
+    'list': (req, res) => {
+        db.Movie.findAll()
+            .then(movies => {
+                res.render('moviesList.ejs', {movies})
+            })
+    },
+    'detail': (req, res) => {
+        db.Movie.findByPk(req.params.id)
+            .then(movie => {
+                res.render('moviesDetail.ejs', {movie});
+            });
+    },
+    'new': (req, res) => {
+        Movies.findAll({
+            order : [
+                ['release_date', 'DESC']
+            ],
+            limit: 5
+        })
+            .then(movies => {
+                res.render('newestMovies', {movies});
+            });
+    },
+    'recomended': (req, res) => {
+        Movies.findAll({
+            where: {
+                rating: {[db.Sequelize.Op.gte] : 8}
+            },
+            order: [
+                ['rating', 'DESC']
+            ]
+        })
+            .then(movies => {
+                res.render('recommendedMovies.ejs', {movies});
+            });
+    },
+    //Aqui dispongo las rutas para trabajar con el CRUD
+    add: function (req, res) {
+        Genres.findAll()
+        .then(genres => {
+            res.render('moviesAdd', {allGenres: genres}) 
+        })
+    },
+    create: function (req,res) {
+        Movies.create({
+            title: req.body.title,
+            rating: req.body.rating,
+            awards: req.body.awards,
+            release_date: req.body.release_date,
+            length: req.body.length,
+            genre_id: req.body.genre_id
+        })
+        res.redirect('/movies');
+
+    },
+    edit: function(req,res) {
+        let peliPk = Movies.findByPk(req.params.id);
+        let dataGenres = Genres.findAll();
+        Promise.all([peliPk, dataGenres])
+        .then(function([Movie, allGenres]){
+            res.render('moviesEdit', {Movie: Movie, allGenres: allGenres})
+        }).catch(err => console.log(err))
+
+    },
+    update: function (req,res) {
+        Movies.update({
+            title: req.body.title,
+            rating: req.body.rating,
+            awards: req.body.awards,
+            release_date: req.body.release_date,
+            length: req.body.length,
+            genre_id: req.body.genre_id
+        },{
+            where: {
+                id: req.params.id
+            }})
+        res.redirect('/movies')
+    },
+    delete: function (req,res) {
+        Movies.findByPk(req.params.id)
+        .then(Movie => {
+            res.render('moviesDelete', {Movie: Movie})})
+        .catch(err => console.log(err))
+    },
+    destroy: function (req,res) {
+        Movies.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        res.redirect('/movies')
+    }
+}
+
+module.exports = moviesController;
